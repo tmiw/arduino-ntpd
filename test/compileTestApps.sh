8bf725331eea83bb7a70d279878ebd142a93f6af
Mooneer Salem <mooneer@gmail.com>
2013-06-06 18:33:42 -0700

    Directory restructuring to support the Arduino IDE.

--- test/compileTestApps.sh
+++ test/compileTestApps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Run this script from the test folder to compile the test suite.
-echo g++ -g -o PCTimeSourceTest -I../src -I../libraries ../libraries/TinyGPS/*.cpp ../src/common/*.cpp ../src/pc/*.cpp PCTimeSourceTest.cpp
-g++ -g -o PCTimeSourceTest -I../src -I../libraries ../libraries/TinyGPS/*.cpp ../src/common/*.cpp ../src/pc/*.cpp PCTimeSourceTest.cpp
+echo g++ -g -o PCTimeSourceTest -I../ -I../libraries ../libraries/TinyGPS/*.cpp ../*.cpp PCTimeSourceTest.cpp
+g++ -g -o PCTimeSourceTest -I../ -I../libraries ../libraries/TinyGPS/*.cpp ../*.cpp PCTimeSourceTest.cpp
